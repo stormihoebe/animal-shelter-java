9d098ebb3bd137c94f660dcb5cae7febbc4ad723
Megan Warnock and Stormi Hoebelheinrich <stormi@>
2017-03-28 14:29:25 -0700

    add first 3 routes and front end logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,24 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/animals/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/animal-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/animals", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("template", "templates/animal-success.vtl");
+    String name = request.queryParams("name");
+    String sex = request.queryParams("sex");
+    String doa = request.queryParams("doa");
+    String type = request.queryParams("type");
+    String breed = request.queryParams("breed");
+    Animal newAnimal = new Animal(name, sex, doa, type, breed);
+    newAnimal.save();
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
   }
 }
