d914b93cf86c0bd02f75be7cc0312e654529025b
Megan Warnock and Stormi Hoebelheinrich <megan@>
2017-03-28 15:20:27 -0700

    add forms and routes for animals and customers

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -34,5 +34,39 @@ public class App {
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
 
+  get("/animals", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("animals", Animal.all());
+    model.put("template", "templates/animals.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  get("/animals/:id", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    Animal animal = Animal.find(Integer.parseInt(request.params(":id")));
+    model.put("animal", animal);
+    model.put("template", "templates/animal.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+
+  get("/customers/new", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("template", "templates/customer-form.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  post("/customers", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("template", "templates/customer-success.vtl");
+    String name = request.queryParams("name");
+    String phone = request.queryParams("phone");
+    String breedPref = request.queryParams("breedPref");
+    String typePref = request.queryParams("typePref");
+    Customer newCustomer = new Customer(name, phone, breedPref, typePref);
+    newCustomer.save();
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
   }
 }
+// $animal.getDoa().substring(0,10)
