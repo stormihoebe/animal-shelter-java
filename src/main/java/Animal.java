c5f435ac638133a19f03219181968ac4dd10e039
Megan Warnock and Stormi Hoebelheinrich <megan@>
2017-03-28 12:08:27 -0700

    add methods

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -6,22 +6,87 @@ import java.time.LocalDateTime;
 public class Animal {
   private String name;
   private String sex;
-  private LocalDateTime doa;
+  private String doa;
   private String type;
   private String breed;
+  private int id;
   private int owner_id;
 
-  public Animal(String name, String sex, String type, String breed) {
+  public Animal(String name, String sex, String doa, String type, String breed) {
     this.name = name;
     this.sex = sex;
-    doa = LocalDateTime.now();
+    this.doa = doa;
     this.type = type;
     this.breed = breed;
+    owner_id = 0;
+
   }
 
   public String getName(){
     return name;
   }
 
+  public String getSex(){
+    return sex;
+  }
+
+  public String getDoa(){
+    return doa;
+  }
+
+  public String getType(){
+    return type;
+  }
+
+  public String getBreed(){
+    return breed;
+  }
+
+  public int getOwnerId(){
+    return owner_id;
+  }
+
+  public void assignOwner(int ownerId){
+    this.owner_id = ownerId;
+    try(Connection con =  DB.sql2o.open()) {
+      String sql = "UPDATE animals SET owner_id = :owner_id WHERE id = :id";
+      con.createQuery(sql)
+      .addParameter("owner_id", owner_id)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
+
+  public void save(){
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO animals (name, sex, doa, type, breed, owner_id) VALUES (:name, :sex, CAST(:doa as TIMESTAMP), :type, :breed, :owner_id)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .addParameter("sex", this.sex)
+      .addParameter("doa", this.doa)
+      .addParameter("type", this.type)
+      .addParameter("breed", this.breed)
+      .addParameter("owner_id", this.owner_id)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static List<Animal> all() {
+    String sql = "SELECT * FROM animals";
+    try(Connection con = DB.sql2o.open()){
+      return con.createQuery(sql).executeAndFetch(Animal.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherAnimal) {
+    if (!(otherAnimal instanceof Animal)) {
+      return false;
+    } else {
+      Animal newAnimal = (Animal) otherAnimal;
+      return this.getName().equals(newAnimal.getName());
+    }
+  }
 
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -20,14 +20,75 @@ public class AnimalTest {
 
   @Test
   public void Animal_instantiatesCorrectly_true(){
-    Animal testAnimal = new Animal("Spock", "male", "dog", "corgi");
+    Animal testAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
     assertEquals(true, testAnimal instanceof Animal);
   }
 
   @Test
   public void getName_instantiatesWithName_true(){
-    Animal testAnimal = new Animal("Spock", "male", "dog", "corgi");
+    Animal testAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
     assertEquals("Spock", testAnimal.getName());
+  }
+
+  @Test
+  public void getSex_instantiatesWithSex_true(){
+    Animal testAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    assertEquals("male", testAnimal.getSex());
+  }
+
+  @Test
+  public void getDoa_instantiatesDateAndTime_true(){
+    Animal testAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    assertEquals("2017-03-25", testAnimal.getDoa());
+  }
+
+  @Test
+  public void getType_instantiatesWithType_true(){
+    Animal testAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    assertEquals("dog", testAnimal.getType());
+  }
+
+  @Test
+  public void getBreed_instantiatesWithBreed_true(){
+    Animal testAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    assertEquals("corgi", testAnimal.getBreed());
+  }
+
+  @Test
+  public void getOwnerId_instantiatesWithOwnerIDOf0ByDefault_0(){
+    Animal testAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    assertEquals(0, testAnimal.getOwnerId());
+  }
+
+  @Test
+  public void equals_returnsTrueIfAnimalsAreTheSame_true() {
+    Animal firstAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    Animal secondAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    assertTrue(firstAnimal.equals(secondAnimal));
+  }
+
+  @Test
+  public void save_savesAnimal() {
+    Animal testAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    testAnimal.save();
+    assertTrue(Animal.all().get(0).equals(testAnimal));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfAnimal_true() {
+    Animal firstAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    firstAnimal.save();
+    Animal secondAnimal = new Animal("Bob", "male", "2016-03-25", "cat", "domestic shorthair");
+    secondAnimal.save();
+    assertEquals(true, Animal.all().get(0).equals(firstAnimal)); assertEquals(true, Animal.all().get(1).equals(secondAnimal));
+  }
+
+  @Test
+  public void assignOwner_updateAssingOwnerID_1(){
+    Animal testAnimal = new Animal("Spock", "male", "2017-03-25", "dog", "corgi");
+    testAnimal.save();
+    testAnimal.assignOwner(1);
+    assertEquals(1, testAnimal.getOwnerId());
   }
 
 }
